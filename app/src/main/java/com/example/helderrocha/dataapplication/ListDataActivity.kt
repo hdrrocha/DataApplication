5fe5cf3b90975f9aa43f0530a96a419ae2b5c2e0
Helder Rocha <hedie.strokes@gmail.com>
2018-08-21 12:14:05 -0300

    initial files

---
+++ app/src/main/java/com/example/helderrocha/dataapplication/ListDataActivity.kt
@@ -0,0 +1,12 @@
+package com.example.helderrocha.dataapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_data_activity)
+    }
+}
